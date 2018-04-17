9789aecc1b01017daec95a9eedf7ae8e50b247bb
Matthew Fitzsimmons <matt@fitzage.com>
2018-04-17 10:25:41 -0700

    fix: converted 404 page to stateless functional component

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
 /**
  * @description Displays error page
  * @constructor
  */
-class NotFound extends Component {
-  render() {
-    return (
-      <div>
-        <h1>404 Not Found</h1>
-        <p>This page doesn't exist.</p>
-      </div>
-    );
-  }
-}
+const NotFound = function(props) {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>This page doesn't exist.</p>
+    </div>
+  );
+};
 
 export default NotFound;
